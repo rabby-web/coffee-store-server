c0f9ce0842c49b45be18eecb253a746468cda631
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-14 23:39:22 +0600

    user related api

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
     // for all
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    const userCollection = client.db("coffeeDB").collection("user");
 
     // get (read coffee)
     app.get("/coffee", async (req, res) => {
@@ -77,6 +78,31 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // user related api -------------------------
+
+    // get
+    app.get("/user", async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // post
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // delete
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
 
